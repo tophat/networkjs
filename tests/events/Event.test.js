5a2caa94b4c7664ad74e7db799d47065e0ccb164
Jack Cohen <jack.cohen@tophatmonocle.com>
2019-06-17 22:19:07 -0400

    Add event tests

---
+++ tests/events/Event.test.js
@@ -0,0 +1,25 @@
+import Event from '../../src/events/Event'
+
+const eventName = 'test'
+
+describe('Event', () => {
+    let e
+    beforeEach(() => {
+        e = new Event(eventName)
+    })
+
+    describe('constructor', () =>{
+        it('initializes with the correct props', () => {
+            expect(e).toMatchSnapshot()
+        })
+    })
+
+    describe('registerCallback', () =>{
+        it('adds callback to event callbacks', () => {
+            const callback = jest.fn()
+            e.registerCallback(callback)
+
+            expect(e.callbacks).toBe([callback])
+        })
+    })
+})
---
+++ tests/events/EventEmitter.test.js
@@ -0,0 +1,50 @@
+import Event from '../../src/events/Event'
+import EventEmitter from '../../src/events/EventEmitter'
+
+const eventName = 'test'
+
+describe('EventEmitter', () => {
+
+    let emitter
+    beforeEach(() => {
+        emitter = new EventEmitter()
+    })
+
+    describe('constructor', () =>{
+        it('initializes with the correct props', () => {
+            expect(emitter).toMatchSnapshot()
+        })
+    })
+
+    describe('registerEvent', () =>{
+        it('adds event to map', () => {
+            emitter.registerEvent(eventName)
+
+            expect(emitter.events[eventName]).toBe(new Event(eventName))
+        })
+    })
+
+    describe('addEventListener', () =>{
+        it('adds event callback to map', () => {
+            const callback = jest.fn()
+            emitter.registerEvent(eventName)
+            emitter.addEventListener(eventName, callback)
+
+            expect(emitter.events[eventName].callbacks).toBe([callback])
+        })
+    })
+
+    describe('dispatchEvent', () =>{
+        it('calls event callbacks', () => {
+            const callback1 = jest.fn()
+            const callback2 = jest.fn()
+            emitter.registerEvent(eventName)
+            emitter.addEventListener(eventName, callback1)
+            emitter.addEventListener(eventName, callback2)
+            emitter.dispatchEvent(eventName)
+
+            expect(callback1).toHaveBeenCalled()
+            expect(callback2).toHaveBeenCalled()
+        })
+    })
+})
