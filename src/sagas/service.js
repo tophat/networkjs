00cf18406e2bcd9fec38a76c0f71e37398cb6125
Jack Cohen <jack.cohen@tophatmonocle.com>
2019-06-23 20:55:47 -0400

    clean up saga

--- src/sagas/service.js
+++ src/sagas/service.js
@@ -13,17 +13,18 @@ class ServiceSaga {
         ) {
             this.isPrefixArray = true
             this.regexes = prefixes.map(p => new RegExp(p))
-            this.failureCounts = prefixes.reduce((obj, p) => {
-                obj[p] = 0
-            }, {})
         } else if (prefixes === '*') {
             this.isPrefixArray = false
-            this.failureCount = 0
         } else {
             throw new Error(`prefixes must be an array or '*'`)
         }
         this.statuses = statuses
-        this.failureThreshold = failureThreshold
+
+        if (typeof failureThreshold === 'number') {
+            this.failureThreshold = failureThreshold
+        } else {
+            throw new Error(`failureThreshold must be a number`)
+        }
         this.decrementTime = decrementTime
 
         this.initialize()
@@ -31,6 +32,7 @@ class ServiceSaga {
 
     initialize() {
         this.paused = false
+        this.failureCounts = {}
         if (this.isPrefixArray) {
             for (let i = 0; i < this.prefixes.length; i++) {
                 this.failureCounts[this.prefixes[i]] = 0
@@ -48,51 +50,52 @@ class ServiceSaga {
                 return this.prefixes[i]
             }
         }
+        return undefined
     }
 
-    getCountForUrl(url) {
+    getCountForPrefix(prefix) {
         return this.isPrefixArray
-            ? this.failureCounts[this.getPrefixForUrl(url)]
+            ? this.failureCounts[prefix]
             : this.failureCount
     }
 
-    increment(url) {
+    increment(prefix) {
         if (this.isPrefixArray) {
-            this.failureCounts[this.getPrefixForUrl(url)]++
+            this.failureCounts[prefix]++
         } else {
             this.failureCount++
         }
     }
 
-    decrement(url) {
+    decrement(prefix) {
         if (this.isPrefixArray) {
-            this.failureCounts[this.getPrefixForUrl(url)]--
+            this.failureCounts[prefix]--
         } else {
             this.failureCount--
         }
     }
 
+    waitAndDecrement(prefix) {
+        setTimeout(() => {
+            if (!this.paused) {
+                this.decrement(prefix)
+                const count = this.getCountForPrefix(prefix)
+                if (count === this.failureThreshold - 1) {
+                    this.emitter.dispatchEvent(NetworkStatus.RESOLVED, prefix)
+                }
+            }
+        }, this.decrementTime)
+    }
+
     handleError(url, status) {
         if (!this.paused && this.statuses.includes(status)) {
-            this.increment()
-            if (this.getCountForUrl(url) >= this.failureThreshold) {
-                this.emitter.dispatchEvent(
-                    NetworkStatus.DEGRADED,
-                    this.getPrefixForUrl(url),
-                )
+            const prefix = this.getPrefixForUrl(url)
+            this.increment(prefix)
+            const count = this.getCountForPrefix(prefix)
+            if (count === this.failureThreshold) {
+                this.emitter.dispatchEvent(NetworkStatus.DEGRADED, prefix)
             }
-
-            setTimeout(() => {
-                if (!this.paused) {
-                    this.decrement()
-                    if (this.getCountForUrl(url) < this.failureThreshold) {
-                        this.emitter.dispatchEvent(
-                            NetworkStatus.RESOLVED,
-                            this.getPrefixForUrl(url),
-                        )
-                    }
-                }
-            }, this.decrementTime)
+            this.waitAndDecrement(prefix)
         }
     }
 
