66b83610dacb9430f36dfc484065130d0a045c7b
Nathan Laundry <nathanlaundry@rogers.com>
2019-12-28 19:35:06 -0500

    test: Add Network monitor tests

---
+++ src/monitors/network/network.test.js
@@ -0,0 +1,48 @@
+import NetworkMonitor from '.'
+import EventEmitter from '../../events/EventEmitter'
+
+describe('Network Monitor', () => {
+    describe('NetworkMonitor', () => {
+        let monitor
+        const emitter = new EventEmitter()
+
+        beforeEach(() => {
+            monitor = new NetworkMonitor(emitter)
+        })
+
+        describe('constructor', () => {
+            it('initializes with the correct props', () => {
+                expect(monitor).toMatchSnapshot()
+            })
+        })
+
+        describe('initialize', () => {
+            it('adds two event listeners to the window', () => {
+                window.addEventListener = jest.fn()
+                monitor.initialize()
+
+                expect(window.addEventListener).toHaveBeenCalledTimes(2)
+            })
+        })
+
+        describe('pause', () => {
+            it('removes two event listeners to the window', () => {
+                window.removeEventListener = jest.fn()
+                monitor.pause()
+
+                expect(window.removeEventListener).toHaveBeenCalledTimes(2)
+            })
+        })
+
+        describe('resume', () => {
+            it('runs initalize which adds two event listeners', () => {
+                window.addEventListener = jest.fn()
+                const initializeSpy = jest.spyOn(monitor, 'initialize')
+                monitor.resume()
+
+                expect(window.removeEventListener).toHaveBeenCalledTimes(2)
+                expect(initializeSpy).toHaveBeenCalled()
+            })
+        })
+    })
+})
